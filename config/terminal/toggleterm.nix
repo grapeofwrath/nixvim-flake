e0d659aba1d72e6c0752242e97d9ece02aa2a8bb
grapeofwrath <69535018+grapeofwrath@users.noreply.github.com>
2024-02-20 19:32:02 -0600

    adjusted toggleterm, removed gruvbox and transparency

--- config/terminal/toggleterm.nix
+++ config/terminal/toggleterm.nix
@@ -1,8 +1,17 @@
 {
   plugins.toggleterm = {
     enable = true;
-    size = 15;
-    openMapping = "<c-t>";
-    direction = "horizontal";
+    size = 12;
+    shadeTerminals = true;
+    shadingFactor = 40;
   };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>tt";
+      action = "<cmd>ToggleTerm direction=horizontal<CR>";
+      options.desc = "Toggle Terminal";
+    }
+  ];
 }
--- config/visuals/theme/default.nix
+++ config/visuals/theme/default.nix
@@ -1,6 +1,6 @@
 {
   imports = [
     ./rose-pine.nix
-    ./transparent-background.nix
+    #./transparent-background.nix
   ];
 }
